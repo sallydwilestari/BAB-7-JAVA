acfb9ebc132e0bb140f8d48fbcb759341650c951
sallydwilestari <sallydwilestari174@gmail.com>
2025-08-07 18:43:46 +0700

    Add files via upload

---
+++ projectbaru.bab 7/src/projectbaru/bab/pkg7/ProjectbaruBab7.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectbaru.bab.pkg7;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Dell
+ */
+public class ProjectbaruBab7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//    String nama[] = new String[3];
+//    nama[0] = "Sally";
+//    nama[1] = "Kaila";
+//    nama[2] = "3D";
+//
+//    System.out.println("Index 0 : "+nama[0]);
+//    System.out.println("Index 1 : "+nama[1]);
+//    System.out.println("Index 2 : "+nama[2]);
+//{ String[] nama = new
+//String[3];
+//nama[0] = JOptionPane.showInputDialog(null,"Nama :","index ke-0 ",
+//        JOptionPane.INFORMATION_MESSAGE);
+//nama[1] = JOptionPane.showInputDialog(null,"Nama :","index ke-1 ",
+//JOptionPane.INFORMATION_MESSAGE);
+//nama[2] = JOptionPane.showInputDialog(null,"Nama :","index ke-2 ",
+//JOptionPane.INFORMATION_MESSAGE);
+//
+//JOptionPane.showMessageDialog(null, nama[0]+", "+nama[1]+", "+nama[2]);6
+//   String umur[][] = new String[2][2];
+//   umur[0][0] = "Sally";
+//   umur[0][1] = "16";
+//   umur[1][0] = "Eko";
+//   umur[1][1] = "25";
+//
+//System.out.println("Nama : "+umur[0][0]+" umur : "+umur[0][1]);
+//System.out.println("Nama : "+umur[1][0]+" umur : "+umur[1][1]);
+    }   
+}
+    
+    
